fdf4d845e5caba9ba9d4f3c5053d8f70c992f723
Tzuzul <mail@tzuzulcode.com>
2022-05-16 10:09:48 -0500

    Configuracion Cloud Run

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose")
 
 async function connection(){
-    const conn = await mongoose.connect("mongodb://mongo/myDB")
-    console.log(conn)
+    
+    const conn = await mongoose.connect("mongodb+srv://tzuzul:b5XPUa5zkYhPeAJ6@backendadvancedabril202.aaldh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+
     console.log("Connection established...")
+    console.log(conn.connection.host)
 }
 
 
