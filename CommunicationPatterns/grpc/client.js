e8f44aea83184c394db0155d62d5def651ced0fa
Tzuzul <mail@tzuzulcode.com>
2022-07-04 11:39:40 -0500

    gRPC

--- CommunicationPatterns/grpc/client.js
+++ CommunicationPatterns/grpc/client.js
@@ -1,15 +1,26 @@
 const grpc = require("@grpc/grpc-js")
+// Generación dinámica:
 // const productsProto = require("./protos/products")
 
 // const client = new productsProto.Products("localhost:4000",grpc.credentials.createInsecure())
+// const idProduct = {
+//     value:"abc123"
+// }
 
-const productsProto = require("./Products_grpc_pb").ProductsClient
 
-const client = new productsProto("localhost:4000",grpc.credentials.createInsecure())
-console.log(client)
+// Generación estática:
+const messages = require("./Products_pb")
+const services = require("./Products_grpc_pb")
 
-const idProduct = {
-    value:"abc123"
-}
+const client = new services.ProductsClient("localhost:4000",grpc.credentials.createInsecure())
 
-console.log(client.getProduct(idProduct))
+const idProduct = new messages.ProductID()
+idProduct.setValue("abc123")
+
+client.getProduct(
+    idProduct,
+    (err,result)=>{
+        console.log(err)
+        console.log(result.getId())
+    }
+)
--- CommunicationPatterns/grpc/services/products.js
+++ CommunicationPatterns/grpc/services/products.js
@@ -1,21 +1,39 @@
-const productsProto = require("../protos/products")
+// Generación dinámica
+// const productsProto = require("../protos/products")
+// const productsService = productsProto.Products.service
 
-const productsService = productsProto.Products.service
+// Generación estática
+const messages = require('../Products_pb');
+const services = require('../Products_grpc_pb');
+const productsService = services.ProductsService
 
 const methods = {
-    addProduct(product){
-        console.log(product)
-        return {
+    addProduct(request,response){
+        const idProduct = request.request.getValue()
+        console.log(idProduct)
+        // response(error,result)
+        response(null,{
             value:"abc123"
-        }
+        })
     },
-    getProduct(id){
-        console.log(id)
-        return {
-            id:"abc123",
-            name:"Producto 1",
-            description:"Descripción del producto"
-        }
+    getProduct(request,response){
+        console.log("idProduct",request.request.getValue())
+        const product = new messages.Product()
+        console.log("Product",product)
+        product.setId("abc123")
+        product.setName("Producto 1") 
+        product.setDescription("Descripción del producto")
+
+        // Dinámico
+        // response(null,{
+        //     id:"abc123",
+        //     name:"Producto 1",
+        //     description:"Descripción del producto"
+        // })
+
+
+        // Estático
+        response(null, product)
     }
 }
 
